159187387d606c7d2fcb688e61e92146808aa811
Minna Yu <minyu@chapman.edu>
2020-11-29 14:02:25 -0800

    adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# numbers.sh
+# Minna Yu
+
+echo "Enter a number: "
+read $NUM
+declare -i counter
+while [[ $counter -le $NUM ]]
+do	
+	zero="0";
+	if [[ $((NUM%2)) == $zero ]];
+	then
+		echo "$counter Even"
+	else
+		echo "$counter Odd"
+	fi
+	let "counter+=1"
+	
+done
