002fb595c1662115e024362d5fd3723ab8579c96
Minna Yu <minyu@chapman.edu>
2020-11-29 18:37:54 -0800

    updates while loop

--- numbers.sh
+++ numbers.sh
@@ -2,18 +2,17 @@
 # numbers.sh
 # Minna Yu
 
-echo "Enter a number: "
-read "$NUM"
-declare -i counter
-while [[ "$counter" -le "$NUM" ]]
+echo "Please enter a positive number: "
+read -r NUM
+counter=1
+while [ "$counter" -le "$NUM" ]
 do	
-	zero="0";
-	if [[ $((NUM%2)) == "$zero" ]];
+	if [ $((NUM%2)) -eq "$zero" ];
 	then
 		echo "$counter Even"
 	else
 		echo "$counter Odd"
 	fi
-	let "counter+=1"
+	N=$((N+1))
 	
 done
